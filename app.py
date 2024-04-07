69d7d111ccca9b474d3c039a50c3ea110b0a9104
OMpatel018 <162850079+OMpatel018@users.noreply.github.com>
2024-04-07 02:18:42 -0400

    "Welcome to the Employee Management System!"

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-"""
-Employee Management System Module
-
-This module provides various functionalities for managing employee data, 
-including adding, deleting, updating, and generating reports.
-"""
-import src.menu as menu
-
 def main():
-    """
-    Main function to start the Employee Management System.
-    
-    This function calls the main_menu() function to display the main menu
-    of the Employee Management System to the user.
-    """
-    menu.main_menu()
+    print("Welcome to the Employee Management System!")
 
 if __name__ == "__main__":
     main()
